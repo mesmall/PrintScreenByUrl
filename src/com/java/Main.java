c88fa889a35a9b9ed1ea94abd9b3870ff4e087f8
lizhen <2225213990@qq.com>
2017-10-12 10:47:28 +0800

    调整像素大小

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -6,9 +6,15 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
+        Runtime rt = Runtime.getRuntime();
+
+        System.out.println("start虚拟机空闲内存："+rt.freeMemory());
+        System.out.println("start虚拟机内存总量："+rt.totalMemory());
+        System.out.println("start虚拟机空闲内存占比："+rt.freeMemory()*1.0/rt.totalMemory());
+
+
         File directory = new File("");//设定为当前文件夹
         String path = directory.getAbsolutePath()+"/src/phantomjs/webpage.js";//获取绝对路径
-        Runtime rt = Runtime.getRuntime();
         String exec = "/usr/local/phantomjs-2.1.1-macosx/bin/phantomjs "+path;
         String urls [] ={
                 "http://www.baidu.com"
@@ -26,7 +32,22 @@ public class Main {
                 System.out.println(tmp);
             }
             System.out.println(sbf.toString());
+            System.out.println("running虚拟机空闲内存："+rt.freeMemory());
+            System.out.println("running虚拟机内存总量："+rt.totalMemory());
+            System.out.println("running虚拟机空闲内存占比："+rt.freeMemory()*1.0/rt.totalMemory());
+            is.close();
+            sbf=null;
+            directory=null;
+            System.out.println("process："+p.isAlive());
+            p.destroy();
+            if(p.isAlive()){
+                p.destroyForcibly();
+                System.out.println("再次销毁："+p.isAlive());
+            }
         }
+        System.out.println("over虚拟机空闲内存："+rt.freeMemory());
+        System.out.println("over虚拟机内存总量："+rt.totalMemory());
+        System.out.println("over虚拟机空闲内存占比："+rt.freeMemory()*1.0/rt.totalMemory());
         return ;
     }
 }
